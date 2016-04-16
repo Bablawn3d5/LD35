011da7aa09465fee422d9929f166caf3c6c12cfe
Stephen Ma <tehphen@gmail.com>
2016-04-16 09:45:58 -0700

    Add random algorthim for spawning blocks

--- LD35GAME/include/Systems/SpawnSystem.h
+++ LD35GAME/include/Systems/SpawnSystem.h
@@ -3,11 +3,21 @@
 #pragma once
 
 #include <entityx/entityx.h>
+#include <Farquaad/Core/EntitySerializer.h>
+#include <random>
 
 namespace ex = entityx;
 
 struct SpawnSystem : public ex::System<SpawnSystem> {
 public:
-  explicit SpawnSystem();
+  explicit SpawnSystem(int limitRow, int limitColumn, EntitySerializer es, unsigned int startSeed = 42) :
+    limitRow(limitRow), limitCol(limitColumn), seed(startSeed), blockCreator(es) {
+    std::mt19937 gen(seed);
+  };
   void update(ex::EntityManager &em, ex::EventManager &events, ex::TimeDelta dt);  // NOLINT
+
+private:
+  EntitySerializer blockCreator;
+  int limitCol, limitRow;
+  unsigned int seed;
 };
--- LD35GAME/src/Systems/SpawnSystem.cpp
+++ LD35GAME/src/Systems/SpawnSystem.cpp
@@ -1,10 +1,41 @@
 // Copyright 2016 Bablawn3d5
 #include <Systems/SpawnSystem.h>
+#include <Components/GameBody.hpp>
+#include <Components/BlockWhole.hpp>
 #include <Farquaad/Components.hpp>
 
-SpawnSystem::SpawnSystem() {
-}
-
 void SpawnSystem::update(ex::EntityManager & em,
-                        ex::EventManager & events, ex::TimeDelta dt) {
+                         ex::EventManager & events, ex::TimeDelta dt) {
+  // Check if BlockWhole exists
+  bool needToSpawn = false;
+  em.each<BlockWhole>(
+    [&](ex::Entity entity, BlockWhole &responder) {
+    needToSpawn = true;
+  }
+  );
+
+  // Spawn something within the top limitRow rows of the game.
+  // Spawn stuff within limitColnums columns
+  if ( needToSpawn ) {
+    std::mt19937 gen(seed);
+    seed = gen.max();
+    std::uniform_int_distribution<> dis(1, 6);
+    std::uniform_int_distribution<> randRow(1, limitRow);
+    std::uniform_int_distribution<> randCol(1, limitCol);
+    // Create a block
+
+    int blocksToCreate = dis(gen);
+    ex::Entity movingEnt = em.create();
+    auto blockWhole = movingEnt.assign<BlockWhole>();
+    while ( blocksToCreate > 0 ) {
+      ex::Entity e = em.create();
+      blockCreator.Load(e);
+      auto b = e.component<GameBody>();
+      b->row = randRow(gen);
+      b->column = randCol(gen);
+      blockWhole->blockParts.push_back(e.id());
+      blocksToCreate--;
+    }
+    blockWhole->pivotBlock = blockWhole->blockParts.at(0);
+  }
 }
